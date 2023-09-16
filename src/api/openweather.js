16a84d8fcaf8975e7bfc4fec9205e3b4d9c8e8f0
Mert OKAN <mertokan5020@gmail.com>
2023-09-16 22:33:59 +0300

    vercel forecast not showing problem fixed

--- src/api/openweather.js
+++ src/api/openweather.js
@@ -1,3 +1,3 @@
 export const OPENWEATHER_API = 'db6c2a00de491a7370fcb41710e0d55b'
 export const OPENWEATHER_URL = `https://api.openweathermap.org/data/2.5`
-export const OPENWEATHER_FORECAST_URL = `http://api.openweathermap.org/data/2.5`
+export const OPENWEATHER_FORECAST_URL = `https://api.openweathermap.org/data/2.5`
