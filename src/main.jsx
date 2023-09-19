51d00c4be7372212f2eaad245f851a02b854472b
Mert OKAN <mertokan5020@gmail.com>
2023-09-19 10:16:30 +0300

    vercel analytics add

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 // main.tsx or main.jsx
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {NextUIProvider} from '@nextui-org/react'
+import {inject} from '@vercel/analytics'
 import App from './App'
 import './index.css'
 
+inject()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <NextUIProvider>
     <App />
