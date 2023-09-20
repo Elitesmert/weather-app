b49298baf141e5e8146c7abf04132993bf889d9b
Mert OKAN <mertokan5020@gmail.com>
2023-09-20 16:37:03 +0300

    vercel product brach work test

--- src/api/geoApi.js
+++ src/api/geoApi.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'fc4e5edfa9msh64ba36bca0292cfp15c93ajsn86568136518b',
+    'X-RapidAPI-Key': 'fc4e5edfa9msh64ba36bca0292cfp15c93ajsn86568136518be',
     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
   },
 }
