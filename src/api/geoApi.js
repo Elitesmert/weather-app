8132f291518a92e1e8712703983d1423e1c065e9
Mert OKAN <mertokan5020@gmail.com>
2023-09-20 17:04:57 +0300

    api changes

--- src/api/geoApi.js
+++ src/api/geoApi.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'fc4e5edfa9msh64ba36bca0292cfp15c93ajsn86568136518be',
+    'X-RapidAPI-Key': import.meta.env.VITE_X_RapidAPI_Key,
     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
   },
 }
